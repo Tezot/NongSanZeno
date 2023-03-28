3c78362ee47fd78566f43c6d9648cc26247f58a4
tieubob <dinhtruong.tech@gmail.com>
2023-03-28 18:41:17 +0700

    create trang chu

--- NongSanZeno/Controllers/HomeController.cs
+++ NongSanZeno/Controllers/HomeController.cs
@@ -10,10 +10,40 @@ using PagedList.Mvc;
 namespace NongSanZeno.Controllers
 {
     public class HomeController : Controller
-    {       
-        public ActionResult index()
+    {
+        dbNongSanZenoDataContext data = new dbNongSanZenoDataContext();
+
+        private List<tbSanPham> Laysanpham(int count)
+        {
+            return data.tbSanPhams.OrderByDescending(a => a.NgayCapNhat).Take(count).ToList();
+        }
+        public ActionResult TrangChu()
+        {
+            //Lay top 6 san pham ban chay nhat
+            var sanpham = Laysanpham(42);
+            string s = Request.QueryString["s"];
+            if (!string.IsNullOrEmpty(s)) sanpham = data.tbSanPhams.OrderByDescending(a => a.NgayCapNhat).Take(42).Where(w => w.TenSP.Contains(s)).ToList();
+            return View(sanpham);
+        }
+
+        public ActionResult GioiThieu()
         {
             return View();
-        }      
+        }
+
+        public ActionResult TinTuc()
+        {
+            return View();
+        }
+
+        public ActionResult AnToanThucPham()
+        {
+            return View();
+        }
+
+        public ActionResult LienHe()
+        {
+            return View();
+        }
     }
 }
